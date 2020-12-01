e12f56b36578f391b55d3ac2e17c9dc5e153c579
SkyNet214 <niklas20066@gmail.com>
2020-12-01 23:17:35 +0100

    01-12-2020, 23:17

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isPaused = false;
     void Reload()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("r")) {
+        if (Input.GetKeyDown("r"))
+        {
             Reload();
+        }
+
+        if (Input.GetKeyDown("n"))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else if (!isPaused)
+            {
+                PauseGame();
+            }
         }
     }
 }
